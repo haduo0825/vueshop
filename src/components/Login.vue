663db39a2cc844a687c9bd2578174a3fdcdc4d35
haduo0825 <804171362@qq.com>
2021-06-29 18:16:03 +0800

    the login page

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang='less' scoped>
+  .box{
+    background-color: pink;
+    height: 500px;
+    width: 500px;
+    text-align: center;
+    line-height: 500px;
+    border-radius: 30%;
+  }
+</style>
